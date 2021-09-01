b114caa25d61077978b522d552a1e485f27e848a
PaulFrere <paulfrere91@gmail.com>
2021-09-01 18:30:10 +0700

    First

---
+++ src/main/java/ru/zsa/nostromo/NostromoApplication.java
@@ -0,0 +1,13 @@
+package ru.zsa.nostromo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NostromoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NostromoApplication.class, args);
+    }
+
+}
